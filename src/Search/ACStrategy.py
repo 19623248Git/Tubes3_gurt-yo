7279cfb4de7680f255b7b53315fe8315629c6bc9
19623248Git <19623248@std.stei.itb.ac.id>
2025-06-15 20:21:02 +0700

    feat: :sparkles: AC Strategy

--- src/Search/ACStrategy.py
+++ src/Search/ACStrategy.py
@@ -7,16 +7,28 @@ Implementation of the SearchStrategy interface using the AC algorithm.
 """
 class ACStrategy(SearchStrategy):
 
-    def search(self, cv: ExtractCV, pattern: str) -> int:
+        """
+        Search for a pattern in the given CV using the Aho-Corasick algorithm.
+        @param cv: The ExtractCV object containing the CV text.
+        @param pattern: The PATTERNS to be searched, e.g. 'React, React Native, React Native Expo'.
+        @return: The number of matches found.
+        """
+        def search(self, cv: ExtractCV, pattern: str) -> int:
 
-        print("Using Aho-Corasick search algorithm...")
+                print("Using Aho-Corasick search algorithm...")
 
-        ac_instance = AC(pattern, cv)
-        results = ac_instance.search()
+                patterns = [kw.strip().lower() for kw in pattern.split(',') if kw.strip()]
 
-        if(results):
-                print(f"Found {len(results)} matches for pattern '{pattern}' in the CV.")
-                return len(results)
-        else:
-                print(f"No matches found for pattern '{pattern}' in the CV.")
-                return 0
+                ac_instance = AC(patterns[0], cv)
+
+                for pattern in patterns[1:]:
+                        ac_instance.insert_pattern(pattern)
+                
+                results = ac_instance.search()
+
+                if(results):
+                        print(f"Found {len(results)} matches for pattern '{pattern}' in the CV.")
+                        return len(results)
+                else:
+                        print(f"No matches found for pattern '{pattern}' in the CV.")
+                        return 0
